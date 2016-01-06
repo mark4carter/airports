9993cd4b4272dbfae14bee018d651825addc72c5
Mark Carter <mark4carter@gmail.com>
2016-01-05 22:48:29 -0500

    tutorial & boiler

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject my-webapp "0.1.0-SNAPSHOT"
+(defproject my-webapp "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [hiccup "1.0.2"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [com.h2database/h2 "1.3.170"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler my-webapp.handler/app}
   :profiles
--- src/my_webapp/handler.clj
+++ src/my_webapp/handler.clj
@@ -1,11 +1,27 @@
 (ns my-webapp.handler
-  (:require [compojure.core :refer :all]
+  (:require [my-webapp.views :as views]
+  					[compojure.core :as cc]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [compojure.handler :as handler]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
+(cc/defroutes app-routes
+  (cc/GET "/" 
+  	[] 
+  	(views/home-page))
+  (cc/GET "/add-location" 
+  	[] 
+  	(views/add-location-page))
+  (cc/GET "/add-location" 
+  	{params :params} 
+  	(views/add-location-results-page params))
+  (cc/GET "/location/:loc-id" 
+  	[loc-id] 
+  	(views/location-page loc-id))
+  (cc/GET "/all-locations" 
+  	[] 
+  	(views/all-locations-page))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (handler/site app-routes))
